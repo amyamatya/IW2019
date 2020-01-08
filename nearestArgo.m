f500b1a34bd89904982de1a9b5b0d9c4a7e5e792
Amy Amatya <43310546+amyamatya@users.noreply.github.com>
2020-01-08 02:59:29 -0500

    Add files via upload

--- nearestArgo.m
+++ nearestArgo.m
@@ -32,7 +32,7 @@ for i = 101:length(merData)
     end
 end
 
-% 3 - find nearest in same month, each year
+% 3 - find nearest in same month, each year 
 argoYear = year(num2str(argoData(:,2)), 'yyyymmdd');
 merYear = year(num2str(merData(:,2)),'yyyymmdd');
 
@@ -52,6 +52,71 @@ for i = 2000:2019
     end
 end
 
-clear argoMonth merMonth argoYear merYear i j k
+clearvars -except argoData merData merCount distArray
+
+%% Are the nearest Argos close enough?
+
+% average distance between each Mermaid and nearest Argo for each search condition
+[rows, cols] = size(distArray);
+for i = 1:cols
+    for j = 1:merCount(1,2)
+        theDists(j,i) = lldistkm([argoData(distArray(j, 1),3) argoData(distArray(j, 1),4)],[merData(j, 3)...
+            merData(j, 4)]);
+        theDists(j,i) = lldistkm([argoData(distArray(j, 2),3) argoData(distArray(j, 2),4)],[merData(j, 3)...
+            merData(j, 4)]);
+    end
+end
+
+for i = 1:cols
+    mean(i) = mean(theDists(:,i));
+end
+
+
+% example: plot first matchup for both conditions
+filename = gunzip('gshhs_c.b.gz', tempdir);
+shorelines = gshhs(filename{1});
+delete(filename{1})
+levels = [shorelines.Level];
+land = (levels == 1);
+
+% all years
+argoIdx = distArray(1:merCount(1,2), 1);
+merIdx = 1:merCount(1,2);
+latMin = min(min(argoData(argoIdx,3)) , min(merData(merIdx, 3)));
+latMax = max(max(argoData(argoIdx,3)) , max(merData(merIdx, 3)));
+lonMin = min(min(argoData(argoIdx,5)) , min(merData(merIdx, 5)));
+lonMax = max(max(argoData(argoIdx,5)) , max(merData(merIdx, 5)));
+h = worldmap([latMin-1 latMax+1],[lonMin-1 lonMax+1]);
+lg1 = scatterm(argoData(argoIdx,3), argoData(argoIdx,5), 12,'filled','ro');
+lg2 = scatterm(merData(merIdx, 3), merData(merIdx, 5), 12,'filled','bo');
+geoshow(shorelines(land),  'FaceColor', [0.9 0.9 0.9]);
+p = findobj(h,'type','patch'); 
+set(p,'FaceColor',[.6 .7 .95]);
+legend([lg1 lg2],'Argo','Mermaid');
+title("Nearest Argo points, avg distance = " + sprintf('%.03f',mean1) + ' km');
+
+% all years, same month
+argoIdx = distArray(1:merCount(1,2), 2);
+merIdx = 1:merCount(1,2);
+latMin = min(min(argoData(argoIdx,3)) , min(merData(merIdx, 3)));
+latMax = max(max(argoData(argoIdx,3)) , max(merData(merIdx, 3)));
+lonMin = min(min(argoData(argoIdx,5)) , min(merData(merIdx, 5)));
+lonMax = max(max(argoData(argoIdx,5)) , max(merData(merIdx, 5)));
+h = worldmap([latMin-.5 latMax+1],[lonMin-1 lonMax+1]);
+lg1 = scatterm(argoData(argoIdx,3), argoData(argoIdx,5), 12,'filled','ro');
+lg2 = scatterm(merData(merIdx, 3), merData(merIdx, 5), 12,'filled','bo');
+geoshow(shorelines(land),  'FaceColor', [0.9 0.9 0.9]);
+p = findobj(h,'type','patch'); 
+set(p,'FaceColor',[.6 .7 .95]);
+legend([lg1 lg2],'Argo','Mermaid');
+title("Nearest Argo points within same month, avg distance = " + sprintf('%.03f',mean2) + ' km');
+
+clearvars -except argoData merData merCount distArray
+
+
+
+
+
+
 
 
